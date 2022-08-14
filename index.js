b75c4a679422d8ebc4ee4279182202e86f23fdfe
Lewis <lewismunyambu@gmail.com>
2022-08-14 23:20:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logshout(string) {
+    console.log.allCaps(); 
+  }
+  function logWhisper(string){
+    console.log .toLowerCase();
+  }
+  
